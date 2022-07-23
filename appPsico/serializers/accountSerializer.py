70e8dadd9e49ee6b5f67d2a8de810ca03fff0a46
Sebastian Martinez <sebastian01973@hotmail.com>
2022-07-23 18:03:01 -0500

    add serializer

---
+++ appPsico/serializers/accountSerializer.py
@@ -0,0 +1,8 @@
+from appPsico.models.account import Account
+from rest_framework import serializers
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['balance', 'lastChangeData', 'isActive']
+        
---
+++ appPsico/serializers/citySerializer.py
@@ -0,0 +1,7 @@
+from appPsico.models.city import City
+from rest_framework import serializers
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ['id', 'city']
